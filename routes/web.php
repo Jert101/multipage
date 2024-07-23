2c43b3a46828c7bbd14d45114665312181bf390c
Jert101 <jersoncatadman@ckcm.edu.ph>
2024-07-23 15:00:33 +0800

    Initial commit

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,51 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about', function () {
+    // return ('this is About');
+    return view ('about');
+});
+
+Route::get('/contact', function () {
+    // return ('this is About');
+    return view ('contact');
+});
+
+Route::get('/pricing', function () {
+    // return ('this is About');
+    return view ('pricing');
+});
+
+
+Route::get('/faq', function () {
+    // return ('this is About');
+    return view ('faq');
+});
+
+Route::get('/bloghome', function () {
+    // return ('this is About');
+    return view ('bloghome');
+});
+
+Route::get('/blogpost', function () {
+    // return ('this is About');
+    return view ('blogpost');
+});
+
+Route::get('/portfoliooverview', function () {
+    // return ('this is About');
+    return view ('portfoliooverview');
+});
+
+Route::get('/portfolioitem', function () {
+    // return ('this is About');
+    return view ('portfolioitem');
+});
+
+Route::get('/welcome', function () {
+    // return ('this is About');
+    return view ('welcome');
+});
+
+
